4583985ebc26e9bede93a1602d73cf7da83acab9
ItsMeTomTom <117825828+ItsMeTomTom@users.noreply.github.com>
2024-03-09 14:47:11 +0100

    Delete src/main/java/addon/antip2w/modules/AnvilUnescape.java

--- src/main/java/addon/antip2w/modules/AnvilUnescape.java
+++
@@ -1,9 +0,0 @@
-package addon.antip2w.modules;
-
-import meteordevelopment.meteorclient.systems.modules.Module;
-
-public class AnvilUnescape extends Module {
-    public AnvilUnescape() {
-        super(Categories.DEFAULT, "anvil-unescape", "unescapes characters in the anvil rename section");
-    }
-}
