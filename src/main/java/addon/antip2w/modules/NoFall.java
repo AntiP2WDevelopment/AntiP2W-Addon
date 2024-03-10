2449eef54fc101d510b0eaf8ad156257e222b68b
ItsMeTomTom <117825828+ItsMeTomTom@users.noreply.github.com>
2024-03-10 16:24:28 +0100

    works on grim (maybe)

--- src/main/java/addon/antip2w/modules/NoFall.java
+++ src/main/java/addon/antip2w/modules/NoFall.java
@@ -1,14 +1,12 @@
 package addon.antip2w.modules;
 
 import meteordevelopment.meteorclient.events.entity.player.SendMovementPacketsEvent;
+import meteordevelopment.meteorclient.events.world.TickEvent;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.orbit.EventHandler;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 
 public class NoFall extends Module {
-
-    private static double lastY = 0;
-    private static boolean shouldCatch = false;
 
     public NoFall() {
         super(Categories.DEFAULT, "NoFall", "Universal NoFall (works on almost any anticheat).");
@@ -16,13 +14,10 @@ public class NoFall extends Module {
 
 
     @EventHandler
-    public void preMovement(SendMovementPacketsEvent.Pre event) {
-        if (mc.player.fallDistance == 0 && !mc.player.isTouchingWater() && mc.player.isOnGround() && shouldCatch) {
-            mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(mc.player.getX(), lastY+0.01, mc.player.getZ(), false));
-            shouldCatch = false;
-        } else if (mc.player.fallDistance > 3) {
-            shouldCatch = true;
+    private void onPreTick(SendMovementPacketsEvent.Post event) {
+        if (!mc.player.isOnGround() && mc.player.fallDistance > 3f) {
+            mc.getNetworkHandler().sendPacket(new PlayerMoveC2SPacket.Full(mc.player.getX(), mc.player.getY() + Math.random()*0.000001, mc.player.getZ(), mc.player.getYaw(), mc.player.getPitch(), false));
+            mc.player.onLanding();
         }
-        lastY = mc.player.getY();
     }
 }
